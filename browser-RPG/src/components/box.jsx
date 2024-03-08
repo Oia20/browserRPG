5ddd78f1f9c63881154160ad6e0f8129eb693678
Jacob <92279567+Oia20@users.noreply.github.com>
2024-03-08 00:05:53 -0600

    Added two new components for the general ui (store and cave)

---
+++ browser-RPG/src/components/box.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './logo.css'
+
+function Shop() {
+
+}
+
+function Cave() {
+
+}
+
+function Box() {
+    return (
+        <div className="box">
+
+        </div>
+    );
+  }
+  
+export default Box
---
+++ browser-RPG/src/components/logo.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import './logo.css'
+
+
+
+function MyLogo() {
+    return (
+      <img src='Dragon_Reppeler-removebg-preview.png' alt="dragon repeller logo"/>
+    );
+  }
+  export default MyLogo
